5e42a751900a751f109a5ce9b8c55d5fefe2309c
Arpan Dhama <arpandhama2001@gmail.com>
2025-03-23 00:03:59 +0530

    nodemailer setup and tested

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
 
+const mailsender = require("./config/email-config");
+
 const app = express();
 
 app.use(express.json());
@@ -9,6 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Server is listening on port ${ServerConfig.PORT}`);
+    try {
+        const response = await mailsender.sendMail({
+            from: ServerConfig.GMAIL_EMAIL,
+            to: "<email-address>@<domain>.com",
+            subject: "Is the service working ? now as well",
+            text: "Yes it is working",
+        });
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 });
---
+++ src/utils/common/enums.js
@@ -0,0 +1,9 @@
+TICKET_ENUMS = {
+    PENDING: "PENDING",
+    SUCCESS: "SUCCESS",
+    FAILED: "FAILED",
+};
+
+module.exports = {
+    TICKET_ENUMS,
+};
