85364b8bb933ca8851a1524608d42a4844964156
Arpan Dhama <arpandhama2001@gmail.com>
2025-03-23 19:56:32 +0530

    added create ticket api

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
 
-const mailsender = require("./config/email-config");
-
 const app = express();
 
 app.use(express.json());
@@ -11,17 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, async () => {
+app.listen(ServerConfig.PORT, () => {
     console.log(`Server is listening on port ${ServerConfig.PORT}`);
-    try {
-        const response = await mailsender.sendMail({
-            from: ServerConfig.GMAIL_EMAIL,
-            to: "<email-address>@<domain>.com",
-            subject: "Is the service working ? now as well",
-            text: "Yes it is working",
-        });
-        console.log(response);
-    } catch (error) {
-        console.log(error);
-    }
 });
