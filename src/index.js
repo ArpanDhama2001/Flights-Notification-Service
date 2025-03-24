a92ebc7f86e1ccf37338d9f1aa675c1a2b89c866
Arpan Dhama <arpandhama2001@gmail.com>
2025-03-25 00:33:22 +0530

    completed consumption and sending mail

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
+const amqplib = require("amqplib");
+const { EmailService } = require("./services");
+
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue", async (data) => {
+            const object = JSON.parse(`${Buffer.from(data.content)}`);
+            await EmailService.sendEmail(
+                ServerConfig.GMAIL_EMAIL,
+                object.recepientEmail,
+                object.subject,
+                object.text
+            );
+            channel.ack(data);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 const app = express();
 
@@ -9,6 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Server is listening on port ${ServerConfig.PORT}`);
+    await connectQueue();
 });
