dfd6620eff7cbd65398c102a2071b102361337ea
Arpan Dhama <arpandhama2001@gmail.com>
2025-08-02 11:41:27 +0530

    Fix: Model not defined

--- src/index.js
+++ src/index.js
@@ -5,24 +5,24 @@ const amqplib = require("amqplib");
 const { EmailService } = require("./services");
 
 async function connectQueue() {
-    try {
-        const connection = await amqplib.connect("amqp://localhost");
-        const channel = await connection.createChannel();
+  try {
+    const connection = await amqplib.connect("amqp://localhost");
+    const channel = await connection.createChannel();
 
-        await channel.assertQueue("noti-queue");
-        channel.consume("noti-queue", async (data) => {
-            const object = JSON.parse(`${Buffer.from(data.content)}`);
-            await EmailService.sendEmail(
-                ServerConfig.GMAIL_EMAIL,
-                object.recepientEmail,
-                object.subject,
-                object.text
-            );
-            channel.ack(data);
-        });
-    } catch (error) {
-        console.log(error);
-    }
+    await channel.assertQueue("noti-queue");
+    channel.consume("noti-queue", async (data) => {
+      const object = JSON.parse(`${Buffer.from(data.content)}`);
+      await EmailService.sendEmail(
+        ServerConfig.GMAIL_EMAIL,
+        object.recepientEmail,
+        object.subject,
+        object.text
+      );
+      channel.ack(data);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const app = express();
@@ -33,6 +33,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
-    console.log(`Server is listening on port ${ServerConfig.PORT}`);
-    await connectQueue();
+  console.log(`Server is listening on port ${ServerConfig.PORT}`);
+  await connectQueue();
 });
